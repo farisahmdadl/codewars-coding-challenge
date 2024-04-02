821e937bd0134fe4e56fc87870adcb5e4e30f987
Faris Adli <69419695+farisahmdadl@users.noreply.github.com>
2024-04-02 07:26:24 +0700

    Add files via upload

---
+++ if-you-cant-sleep-just-count-sheep/ifYouCantSleepJustCountSheep.js
@@ -0,0 +1,9 @@
+var countSheep = function (num){
+    let sheep = [];
+    for (let i = 1; i <= num; i++) {
+        sheep.push(i + ' sheep...');
+    }
+    return sheep.join('');
+  }
+
+console.log(countSheep(5));
