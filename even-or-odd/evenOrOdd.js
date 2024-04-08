17142d37afa28e73fc5b5fc31020dc4652bcada3
Faris Adli <69419695+farisahmdadl@users.noreply.github.com>
2024-04-08 21:59:04 +0700

    Add files via upload

---
+++ even-or-odd/evenOrOdd.js
@@ -0,0 +1,11 @@
+function evenOrOdd(number) {
+    if (number % 2 != (0)) {
+        return 'Odd';
+    } else {
+        return 'Even';
+    }
+}
+
+console.log(evenOrOdd(-7));
+console.log(evenOrOdd(7));
+console.log(evenOrOdd(8));
