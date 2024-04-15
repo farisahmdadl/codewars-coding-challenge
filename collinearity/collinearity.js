09758a2c330c9df8749b5e90fceb2b01648a9e9b
Faris Adli <69419695+farisahmdadl@users.noreply.github.com>
2024-04-15 09:20:55 +0700

    Add files via upload

---
+++ collinearity/collinearity.js
@@ -0,0 +1,12 @@
+function collinearity( x1,y1,x2,y2 ) {
+    let resultA = x1*y2;
+    let resultB = y1*x2;
+    console.log(resultA);
+    console.log(resultB);
+    return resultA===resultB;
+  }
+
+  console.log(collinearity(1,1,1,1));
+  console.log(collinearity(1,1,0,0));
+  console.log(collinearity(1,1,6,1));
+  console.log(collinearity(4,0,11,0));
