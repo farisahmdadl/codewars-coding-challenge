034f56694c8b426978c894084e99ad8121074d06
Faris Adli <69419695+farisahmdadl@users.noreply.github.com>
2025-01-16 16:42:15 +0700

    Add files via upload

---
+++ name-shuffler/nameShuffler.js
@@ -0,0 +1,7 @@
+function nameShuffler(str){
+    return str.split(" ").reverse().join(" ");;
+  }
+
+  console.log(nameShuffler("Faris Adli"));
+  console.log(nameShuffler("Cahaya Ramadhan"));
+  console.log(nameShuffler("Ayub Alfathoni"))
